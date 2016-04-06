575b0e32f3ac60db458cac8d01e1eb335eea2dfe
Marissa Laliberte <marissalaliberte@Marissas-MacBook-Pro.local>
2016-04-05 22:15:14 -0400

    python exercises

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input('22')
+print "How tall are you?",
+height = raw_input('5\'5')
+print "How much do you weigh?",
+weight = raw_input('120lbs')
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    '22', '5\'5"', '120lbs')
---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you? 22")
+height = raw_input("How tall are you? 5'5\"")
+weight = raw_input("How much do you weigh? 120lbs")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    '22 years', '5 feet 5 inches', '120 pounds')
